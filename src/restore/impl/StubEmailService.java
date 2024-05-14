9393a5b1a596cd0363b302d032e0994e8ad7ed5b
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-05-14 15:46:13 +0200

    Test email service

---
+++ src/restore/impl/StubEmailService.java
@@ -0,0 +1,10 @@
+package restore.impl;
+
+import restore.EmailService;
+
+public final class StubEmailService implements EmailService {
+    @Override
+    public void sendPasswordResetEmail(String email, String code) {
+        System.out.println("Send code = " + code + "to " + email + " email !");
+    }
+}
